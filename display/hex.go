94be9a06a1d9afcc8dfc87441436d9ffbabcad2d
raspi <pekka.jarvinen@gmail.com>
2019-12-29 14:08:42 +0200

    better name

--- display/hex.go
+++ display/hex.go
@@ -7,22 +7,22 @@ import (
 	"strings"
 )
 
-type HexOffset struct {
+type Hex struct {
 	fs      uint8
 	palette map[uint8]clr.Color
 }
 
-func (d HexOffset) SetBitWidthSize(s uint8) {
+func (d Hex) SetBitWidthSize(s uint8) {
 	d.fs = s
 }
 
-func NewHex() *HexOffset {
-	return &HexOffset{
+func NewHex() *Hex {
+	return &Hex{
 		fs: 8,
 	}
 }
 
-func (d HexOffset) Display(a []byte) string {
+func (d Hex) Display(a []byte) string {
 	out := ``
 	for idx, b := range a {
 		if idx == 8 {
@@ -40,17 +40,17 @@ func (d HexOffset) Display(a []byte) string {
 	return strings.Trim(out, ` `)
 }
 
-func (d HexOffset) leading(i int64) string {
+func (d Hex) leading(i int64) string {
 	out := fmt.Sprintf(`%02x`, i)
 	out = strings.Repeat(`0`, int(d.fs-2)-len(out)) + out
 	return out
 }
 
-func (d HexOffset) DisplayOffset(r io.ReadSeeker) string {
+func (d Hex) DisplayOffset(r io.ReadSeeker) string {
 	off, _ := r.Seek(0, io.SeekCurrent)
 	return d.leading(off)
 }
 
-func (d *HexOffset) SetPalette(p map[uint8]clr.Color) {
+func (d *Hex) SetPalette(p map[uint8]clr.Color) {
 	d.palette = p
 }
