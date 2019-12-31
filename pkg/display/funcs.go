646d464e731eee557e49f469f7adcc2a321c608e
raspi <pekka.jarvinen@gmail.com>
2019-12-31 14:45:38 +0200

    not used

--- pkg/display/funcs.go
+++ pkg/display/funcs.go
@@ -2,27 +2,6 @@ package display
 
 import "fmt"
 
-/*
-nearest returns nearest bits to uint8-64 len
-
-1-7 = 8
-9-15 = 16
-
-and so on
-*/
-func nearest(bitWidth uint8) uint8 {
-	if bitWidth > 64 {
-		return 64
-	} else if bitWidth > 32 {
-		return 64
-	} else if bitWidth > 16 {
-		return 32
-	} else if bitWidth > 8 {
-		return 16
-	}
-	return 8
-}
-
 // header returns header for formatters of different lengths
 // for example:
 // - bit formatter displays 8 characters (00000000-11111111)
