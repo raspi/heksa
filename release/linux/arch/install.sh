6c619c582b72a7347d163f0bd124ede1e1c0d3e2
raspi <pekka.jarvinen@gmail.com>
2020-01-17 15:38:53 +0200

    Fix arch pkgbuild

--- release/linux/arch/install.sh
+++ release/linux/arch/install.sh
@@ -1,4 +1,4 @@
-cd "$srcdir"
+cd "$srcdir/$pkgname-$pkgver"
 install -Dm644 "LICENSE" -t "$pkgdir/usr/share/licenses/$pkgname"
 install -Dm644 "README.md" -t "$pkgdir/usr/share/doc/$pkgname"
 install -Dm755 "bin/$pkgname" -t "$pkgdir/usr/bin"
