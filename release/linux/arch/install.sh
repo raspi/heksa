2b62fc15bdf271f3b862ccbb0669a42c6f7d513c
raspi <pekka.jarvinen@gmail.com>
2020-01-21 23:11:46 +0200

    added CoC and contributing to PKGBUILD

--- release/linux/arch/install.sh
+++ release/linux/arch/install.sh
@@ -1,4 +1,6 @@
 cd "$srcdir/$pkgname-$pkgver"
 install -Dm644 "LICENSE" -t "$pkgdir/usr/share/licenses/$pkgname"
 install -Dm644 "README.md" -t "$pkgdir/usr/share/doc/$pkgname"
+install -Dm644 "CODE_OF_CONDUCT.md" -t "$pkgdir/usr/share/doc/$pkgname"
+install -Dm644 "CONTRIBUTING.md" -t "$pkgdir/usr/share/doc/$pkgname"
 install -Dm755 "bin/$pkgname" -t "$pkgdir/usr/bin"
