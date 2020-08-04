d887ead8d07fed9d23c7a61632d7a36d1d2aff62
raspi <pekka.jarvinen@gmail.com>
2020-08-04 21:35:37 +0300

    added comment

--- pkg/reader/offsetFormatters/base/iface.go
+++ pkg/reader/offsetFormatters/base/iface.go
@@ -5,6 +5,8 @@ type OffsetFormatter interface {
 	Print(offset uint64) string
 }
 
+// BaseInfo contains meta information about a file which is read for offset formatters
 type BaseInfo struct {
+	// File size hint for offset formatter(s), for example how many padding zeroes are needed when printing out position
 	FileSize int64
 }
